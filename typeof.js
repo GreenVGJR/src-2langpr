c263d1db470d888f91ea44aadec28faa2617a5a3
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2022-11-08 20:41:26 +0700

    Create typeof.js

---
+++ typeof.js
@@ -0,0 +1,6 @@
+/*
+if you can " $djsEval[typeof(value);yes] "
+why not just do this instead
+*/
+
+$replaceText[$replaceText[$replaceText[$checkCondition[$replaceText[$replaceText[$checkCondtion[$findSpecialChars[$message[1]]==];false;];true;$findChars[$replaceText[$replaceText[$checkContains[$toLowercase[$message[1]];true;false];true;];false;$message[1]]]]!=];false;undefined];true;$replaceText[$replaceText[$checkCondition[$replaceText[$replaceText[$checkContains[$message[1];{;}];true;];false;$findSpecialChars[$message[1]]]==];false;$replaceText[$replaceText[$checkContains[$message[1];(;)];true;function];false;string]];true;$replaceText[$replaceText[$checkCondition[$findNumbers[$message[1]]==];false;number];true;$replaceText[$replaceText[$checkContains[$message[1];{;}];true;object];false;$replaceText[$replaceText[$checkContains[$toLowercase[$message[1]];true;false];true;boolean];false;undefined]];false;undefined];false;undefined];false;undefined];false;undefined]
