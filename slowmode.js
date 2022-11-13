aea193f05ff06ac1230d81279892f5a324f7db48
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2022-11-13 14:08:51 +0700

    Create slowmode.js

---
+++ slowmode.js
@@ -0,0 +1,7 @@
+$slowmode[$mentionedChannels[1;yes];$multi[$message[1];1000]]
+Slowmode has set to **$humanizeMS[$multi[$message[1];1000];10]** on <#$mentionedChannels[1;yes]>
+$onlyIf[$isNumber[$message[1]]!=false;Must number!]
+$argsCheck[>1;Usage: \`slowmode (duration, just number) / slowmode (mention channel) (duration, just number)\`]
+$onlyPerms[managechannels;**Manage Channels** - User]
+$onlyBotPerms[managechannels;**Manage Channels** - Bot]
+$suppressErrors[something just happened.]
