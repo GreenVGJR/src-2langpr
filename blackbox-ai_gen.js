a8b3b0d11874953fcf9ca224e9825c1acb04eb16
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2024-12-01 02:35:14 +0700

    fix bot protection

--- blackbox-ai_gen.js
+++ blackbox-ai_gen.js
@@ -15,6 +15,24 @@ $if[$charCount[$get[c1]]==1;
 $if[$get[c1]==2;$let[model;gpt-4o];
 $if[$get[c1]==3;$let[model;gemini-pro];
 $if[$get[c1]==4;$let[model;claude-sonnet-3.5]]]]]
+
+$let[mid;$sendMessage[$channelID;Generating.;true]]
+
+$c[-- Looking for ID, allowing to use model --]
+$try[
+$let[chttp1;$httpRequest[https://www.blackbox.ai/agent/ImageGenerationLV45LJp;GET;te]]
+$onlyIf[$get[chttp1]==200;$!editMessage[$channelID;$get[mid];$nomention ($get[chttp1]) Script | Can't continue the process.]]
+$let[achievest;$advancedTextSplit[$httpGetHeader[Set-Cookie];\\\;;0]]
+$textSplit[$advancedTextSplit[$env[te];app/layout;0];"]
+$let[scripturl;https://www.blackbox.ai$advancedTextSplit[$env[te];app/layout;0;";$sub[$getTextSplitLength;1]]app/layout$advancedTextSplit[$env[te];app/layout;1;";0]]
+$let[chttp2;$httpRequest[$get[scripturl];GET;tr]]
+$onlyIf[$get[chttp2]==200;$!editMessage[$channelID;$get[mid];$nomention ($get[chttp2]) ValidateID | Can't continue the process.]]
+$let[finalid;$advancedTextSplit[$env[tr];,g={subject:";1;,v=";1;";0]]
+;
+$!editMessage[$channelID;$get[mid];Can't continue the process.]
+$stop
+]
+
 $let[listbody;{
   "messages": \\[
     {
@@ -44,12 +62,19 @@ $let[listbody;{
   "clickedForceWebSearch": false,
   "visitFromDelta": false,
   "mobileClient": false,
-  "userSelectedModel": "$get[model]"
+  "userSelectedModel": "$get[model]",
+  "validated": "$get[finalid]",
+  "imageGenerationMode": false
 }
 ]
-$let[mid;$sendMessage[$channelID;Generating.;true]]
+
+$c[-- Generate Image --]
 $try[
 $httpAddHeader[user-agent;Mozilla/5.0 (Macintosh; U; Intel Mac OS X 8_8_4; en-US) AppleWebKit/537.7 (KHTML, like Gecko) Chrome/52.0.3525.375 Safari/603]
+$httpAddHeader[origin;https://www.blackbox.ai]
+$httpAddHeader[referer;https://www.blackbox.ai/agent/ImageGenerationLV45LJp]
+$httpAddHeader[content-type;text/plain]
+$httpAddHeader[cookie;$get[achievest]]
 $httpSetBody[$get[listbody]]
 $let[http;$httpRequest[https://www.blackbox.ai/api/chat;POST;res]]
 $onlyIf[$get[http]==200;$!editMessage[$channelID;$get[mid];$nomention ($get[http]) Can't continue the process.]]
