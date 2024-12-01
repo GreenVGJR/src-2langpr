d6fcb7b9e8964c39225fd485bab675666ec7a88a
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2024-12-01 17:36:50 +0700

    Update atd.js

--- atd.js
+++ atd.js
@@ -11,16 +11,20 @@ module.exports = [{
    code: `$onlyIf[$message[0]!=;$reply Usage: \`tk-d <url>\`]
 $onlyIf[$checkContains[$message[0];vt.tiktok.com;vm.tiktok.com;www.tiktok.com;tiktok.com]!=false;$reply Invalid link.]
 $onlyIf[$advancedTextSplit[$message[0];https://;1]!=;$reply Invalid link.]
-$let[mid;$sendMessage[$channelID;$reply Getting Player info.;true]]
+$let[mid;$sendMessage[$channelID;$reply Getting Video info.;true]]
 $let[ismobileurl;false]
 $let[ishdvideo;false]
 $let[quality;SD]
 $let[cacherealeurl;$message[0]]
 $if[$checkContains[$message[0];vt.tiktok.com;vm.tiktok.com]==false;
 $let[realurl;$message[0]]
+$let[statusz;$httpRequest[$message[0];HEAD;whichcookie]]
+$onlyIf[$get[statusz]==200;$!editMessage[$channelID;$get[mid];($get[statusz]) Can't process this.] $stop]
+$let[storecookie;$httpGetHeader[Set-Cookie]]
 ;
 $let[statusw;$httpRequest[$message[0];GET;fetchurl]]
 $onlyIf[$get[statusw]==200;$!editMessage[$channelID;$get[mid];($get[statusw]) Can't process this.] $stop]
+$let[storecookie;$httpGetHeader[Set-Cookie]]
 $let[fetchurl;$decodeURI[$replaceText[$replaceText[$advancedTextSplit[$env[fetchurl];"seo.abtest":{"canonical":";1;";0];\\u002F;/];\\\\;]]]
 $let[realurl;$replaceText[$get[fetchurl];/@/video/;/@$advancedTextSplit[$env[fetchurl];"uniqueId":";1;";0]/video/]]
 $let[ismobileurl;true]
@@ -29,26 +33,12 @@ $if[$checkContains[$message;--wm];$let[typecontent;"downloadAddr":"];$let[typeco
 $onlyIf[$checkContains[$message;--wm --lite;--lite --wm]!=true;$!editMessage[$channelID;$get[mid];(404) There's no watermark video for lite version.]]
 $onlyIf[$checkContains[$message;--wm --hd;--hd --wm]!=true;$!editMessage[$channelID;$get[mid];(404) There's no watermark video for HD version.]]
 $let[cookie;$djsEval[
-const axios = require('axios');
-const url = 'https://www.tiktok.com';
-const headers = {
-   'User-Agent': "undici",
-   'Referer': 'https://www.tiktok.com/'
-};
-
-axios.get(url, { headers: headers })
-   .then(response => {
-       const setCookieHeader = response.headers\\['set-cookie'\\];
-       const setCookieString = Array.isArray(setCookieHeader) ? setCookieHeader.join('; ') : setCookieHeader;
-       return setCookieString;
-}).catch(error => {
-   ctx.channel.send("Error: " + error);
-   return null;
-});
+    const setCookieHeader = ctx.getKeyword("storecookie");
+    const setCookieString = Array.isArray(setCookieHeader) ? setCookieHeader.join('; ') : setCookieHeader;
+    setCookieString;
 ]
 ]
 $onlyIf[$get[cookie]!=;$reply Failed retrieve cookies. $stop]
-$!editMessage[$channelID;$get[mid];Getting Video info.]
 $httpAddHeader[user-agent;undici]
 $httpAddHeader[referer;https://www.tiktok.com/]
 $httpAddHeader[cookie;$get[cookie]]
@@ -75,21 +65,22 @@ $let[videoid;$advancedTextSplit[$env[test];"webapp.video-detail":{"itemInfo":{"i
 $let[videoarst;$advancedTextSplit[$env[test];"uniqueId":";1;";0]]
 $let[videocdn;$advancedTextSplit[$env[test];$get[typecontent];1;video\\\\u002Ftos\\\\u002F;1;\\\\;0]]
 $let[copyrightaudio;$advancedTextSplit[$env[test];"err_sound_copy":";1;";0;.;0].]
-$!editMessage[$channelID;$get[mid];Downloading.]
 $!djsEval[
 function extractValidCookiePairs(cookieString) {
-   const segments = cookieString.split(';');
-   const keyValuePairs = segments.filter(segment => {
-       const trimmedSegment = segment.trim();
-       return trimmedSegment.includes('=') && !/path=|domain=|samesite=|secure|httponly|expires=/i.test(trimmedSegment);
-   });
-   return keyValuePairs.join('; ');
+    const cookies = cookieString.split(',');
+    const validCookies = cookies.map(cookie => {
+        const keyValue = cookie.split(';')\\[0\\].trim();
+        return keyValue;
+    });
+
+    return validCookies.join('; ');
 }
 
 function extractTTChainToken(cookieString) {
-   const match = cookieString.match(/tt_chain_token=\\[^;\\]+/);
-   return match ? match\\[0\\] : '';
-}
+    // Use a regex to find the tt_chain_token
+    const match = cookieString.match(/tt_chain_token=(\\[^;\\]+)/);
+    return match ? match\\[1\\] : '';
+};
 
 const axios = require('axios');
 const fs = require('fs');
@@ -98,7 +89,7 @@ const cookie = extractValidCookiePairs(ctx.getKeyword("cookie"));
 const headers = {
    'User-Agent': "undici",
    'Referer': 'https://www.tiktok.com/',
-   'Cookie': extractTTChainToken(cookie),
+   'Cookie': cookie,
    'Accept': 'video/mp4'
 };
 
@@ -143,7 +134,7 @@ try {
 
 
       ctx.channel.messages.fetch(ctx.getKeyword("mid")).then(message => {
-        message.edit('Uploading.');
+        message.edit((ctx.getKeyword("ishdvideo") == "true" ? "Video doesn't play normally? Download it first." : '') + '\\\`\\\`\\\`xml\\\\nQuality   : ' + ctx.getKeyword("quality") + '\\\\nSize      : ' + fileSizeInMegabytes.toFixed(2) + ' MB\\\\nVideoID   : ' + ctx.getKeyword("videoid") + '\\\\nUploader  : ' + ctx.getKeyword("videoarst") + '\\\\nCDN	   : ' + ctx.getKeyword("videocdn") + '\\\\nCookie    : ' + extractTTChainToken(cookie) + '\\\\nCopyright : ' + ctx.getKeyword("copyrightaudio") + (ctx.getKeyword("ismobileurl") == "true" ? '\\\\n\\\\nInput URL : ' + ctx.getKeyword("cacherealeurl") + '\\\\nVideo URL : ' + ctx.getKeyword("realurl") + '\\\\n\\\`\\\`\\\`' : '\\\\n\\\`\\\`\\\`') + '\\\\n\\[Video\\](' + ctx.getKeyword("hls") + ') | \\[Audio\\](' + ctx.getKeyword("mhls") + ')');
       });
 
       ctx.channel.messages.fetch(ctx.getKeyword("mid")).then(message => {
@@ -152,10 +143,6 @@ try {
             attachment: videoBuffer,
             name: 'videott-' + ctx.getKeyword("videoid") + '.mp4'
           }\\]
-        }).then(backup => {
-          const attachmentSizeInBytes = backup.attachments.first().size;
-          const attachmentSizeInMB = attachmentSizeInBytes / (1024 * 1024); // Convert to MB
-          backup.edit((ctx.getKeyword("ishdvideo") == "true" ? "Video doesn't play normally? Download it first." : '') + '\\\`\\\`\\\`xml\\\\nQuality   : ' + ctx.getKeyword("quality") + '\\\\nSize      : ' + attachmentSizeInMB.toFixed(2) + ' MB\\\\nVideoID   : ' + ctx.getKeyword("videoid") + '\\\\nUploader  : ' + ctx.getKeyword("videoarst") + '\\\\nCDN	   : ' + ctx.getKeyword("videocdn") + '\\\\nCookie    : ' + extractTTChainToken(cookie) + '\\\\nCopyright : ' + ctx.getKeyword("copyrightaudio") + (ctx.getKeyword("ismobileurl") == "true" ? '\\\\n\\\\nInput URL : ' + ctx.getKeyword("cacherealeurl") + '\\\\nVideo URL : ' + ctx.getKeyword("realurl") + '\\\\n\\\`\\\`\\\`' : '\\\\n\\\`\\\`\\\`') + '\\\\n\\[Video\\](' + ctx.getKeyword("hls") + ') | \\[Audio\\](' + ctx.getKeyword("mhls") + ')');
         });
       });
     });
