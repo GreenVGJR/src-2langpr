8e495f3ebcefdcde93427a1f0a965bb31a806cf4
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2024-12-13 21:24:37 +0700

    Update blackbox-ai_gen.js

--- blackbox-ai_gen.js
+++ blackbox-ai_gen.js
@@ -3,23 +3,15 @@ name: "img-gen",
 type: "messageCreate",
 code: `$reply[$channelID;$messageID]
 $nomention
-$onlyIf[$message[0]!=;Usage: \`img-gen <prompt> --type=(number)\`\n\`\`\`--type=(number)\n\n1 : BlackBox.AI\n2 : GPT-4o\n3 : Gemini-Pro\n4 : Claude-Sonnet-3.5\`\`\`]
-$onlyIf[$checkContains[$message[1];--type=]!=true;Parameter \`prompt\` should be at first position.]
+$onlyIf[$message[0]!=;Usage: \`img-gen <prompt>\`]
 $onlyIf[$hasPerms[$guildID;$clientID;AttachFiles]!=false;Missing Permission, **Attach Files** - Bot]
 
-$let[content;$replaceText[$advancedTextSplit[$message;--type=;0];
+$let[content;$replaceText[$message;
 ;]]
-$let[c1;$advancedTextSplit[$message;--type=;1; ;0]]
-
-$let[model;blackboxai]
-$if[$charCount[$get[c1]]==1;
-$if[$get[c1]==2;$let[model;gpt-4o];
-$if[$get[c1]==3;$let[model;gemini-pro];
-$if[$get[c1]==4;$let[model;claude-sonnet-3.5]]]]]
 
 $let[mid;$sendMessage[$channelID;Generating.;true]]
 
-$c[-- Looking for ID, allowing to use model --]
+$c[-- Looking for ID --]
 $try[
 $let[chttp1;$httpRequest[https://www.blackbox.ai/agent/ImageGenerationLV45LJp;GET;te]]
 $onlyIf[$get[chttp1]==200;$!editMessage[$channelID;$get[mid];$nomention ($get[chttp1]) Script | Can't continue the process.]]
@@ -28,7 +20,7 @@ $textSplit[$advancedTextSplit[$env[te];app/layout;0];"]
 $let[scripturl;https://www.blackbox.ai$advancedTextSplit[$env[te];app/layout;0;";$sub[$getTextSplitLength;1]]app/layout$advancedTextSplit[$env[te];app/layout;1;";0]]
 $let[chttp2;$httpRequest[$get[scripturl];GET;tr]]
 $onlyIf[$get[chttp2]==200;$!editMessage[$channelID;$get[mid];$nomention ($get[chttp2]) ValidateID | Can't continue the process.]]
-$let[finalid;$advancedTextSplit[$env[tr];,g={subject:";1;,v=";1;";0]]
+$let[finalid;$advancedTextSplit[$env[tr];},b=";1;";0]]
 ;
 $!editMessage[$channelID;$get[mid];Can't continue the process.]
 $stop
@@ -53,6 +45,7 @@ $let[listbody;{
   },
   "trendingAgentMode": {},
   "isMicMode": false,
+  "userSystemPrompt": null,
   "maxTokens": 1024,
   "playgroundTopP": null,
   "playgroundTemperature": null,
@@ -63,9 +56,10 @@ $let[listbody;{
   "clickedForceWebSearch": false,
   "visitFromDelta": false,
   "mobileClient": false,
-  "userSelectedModel": "$get[model]",
+  "userSelectedModel": null,
   "validated": "$get[finalid]",
-  "imageGenerationMode": false
+  "imageGenerationMode": false,
+  "webSearchModePrompt": false
 }
 ]
 
@@ -74,15 +68,13 @@ $try[
 $httpAddHeader[user-agent;Mozilla/5.0 (Macintosh; U; Intel Mac OS X 8_8_4; en-US) AppleWebKit/537.7 (KHTML, like Gecko) Chrome/52.0.3525.375 Safari/603]
 $httpAddHeader[origin;https://www.blackbox.ai]
 $httpAddHeader[referer;https://www.blackbox.ai/agent/ImageGenerationLV45LJp]
-$httpAddHeader[content-type;text/plain]
+$httpAddHeader[content-type;application/json]
 $httpAddHeader[cookie;$get[achievest]]
 $httpSetBody[$get[listbody]]
 $let[http;$httpRequest[https://www.blackbox.ai/api/chat;POST;res]]
 $onlyIf[$get[http]==200;$!editMessage[$channelID;$get[mid];$nomention ($get[http]) Can't continue the process.]]
-$!editMessage[$channelID;$get[mid];$nomention Uploading.]
-$!editMessage[$channelID;$get[mid];$nomention $attachment[https://storage.googleapis.com/$advancedTextSplit[$env[res];https://storage.googleapis.com/;1;);0];image.png]
-Prompt: ||$get[content]||
-Model: $get[model]]
+$!editMessage[$channelID;$get[mid];$nomention $attachment[https://storage.googleapis.com/$advancedTextSplit[$env[res];https://storage.googleapis.com/;1;);0];image.jpeg]
+]
 ;
 $!editMessage[$channelID;$get[mid];Something just happened.]
 ]
