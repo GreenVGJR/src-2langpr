07aef5c89f49ba63b0140f047641ae4cf7bb71e8
GreenVGJR <79613385+GreenVGJR@users.noreply.github.com>
2025-06-19 00:32:11 +0700

    Update ytdl-downloader.js

--- ytdl-downloader.js
+++ ytdl-downloader.js
@@ -25,13 +25,13 @@ $stop
 ;errorhttp]
 
 $jsonLoad[aa;$env[reshttp;streamingData;adaptiveFormats]]
-$jsonLoad[filter_aa;$arrayMap[aa;ab;$if[$and[$jsonHas[ab;isDrc]==false;$jsonHas[ab;audioQuality];$jsonHas[ab;audioTrack]==false];$return[$env[ab]]]]]
+$jsonLoad[filter_aa;$arrayMap[aa;ab;$if[$and[$jsonHas[ab;isDrc]==false;$jsonHas[ab;audioQuality];$or[$jsonHas[ab;audioTrack]==false;$checkContains[$env[ab;audioTrack;displayName];original]]];$return[$env[ab]]]]]
 $arrayReverse[filter_aa;filter_aa]
 
 $author[$env[reshttp;videoDetails;author]]
 $title[$env[reshttp;videoDetails;title];https://www.youtube.com/watch?v=$get[videoid]]
 $footer[$parseDigital[$multi[$env[reshttp;videoDetails;lengthSeconds];1000]]]
-$thumbnail[https://i.ytimg.com/vi/$get[videoid]/sddefault.jpg]
+$thumbnail[https://i.ytimg.com/vi_webp/$get[videoid]/sddefault.webp]
 $color[a09fff]
 
 $addActionRow
@@ -66,7 +66,7 @@ $stop
 ;errorhttp]
 
 $jsonLoad[aa;$env[reshttp;streamingData;adaptiveFormats]]
-$jsonLoad[filter_aa;$arrayMap[aa;ab;$if[$and[$jsonHas[ab;isDrc]==false;$jsonHas[ab;audioQuality];$jsonHas[ab;audioTrack]==false];$return[$if[$checkContains[$env[ab;url];&ratebypass];$env[ab];$replace[$env[ab];&requiressl=yes;&requiressl=yes&ratebypass=true&range=0-$if[$env[ab;contentLength]>=10000000;10000000;$env[ab;contentLength]];1]]]]]]
+$jsonLoad[filter_aa;$arrayMap[aa;ab;$if[$and[$jsonHas[ab;isDrc]==false;$jsonHas[ab;audioQuality];$or[$jsonHas[ab;audioTrack]==false;$checkContains[$env[ab;audioTrack;displayName];original]]];$return[$if[$checkContains[$env[ab;url];&ratebypass];$env[ab];$replace[$env[ab];&requiressl=yes;&requiressl=yes&ratebypass=true&range=0-$if[$env[ab;contentLength]>=10000000;10000000;$env[ab;contentLength]];1]]]]]]
 $jsonLoad[specific_aa;$arrayMap[filter_aa;filter_ab;$if[$env[filter_ab;itag]==$get[itag];$return[$env[filter_ab]]]]]
 
 $onlyIf[$env[specific_aa;0;url]!=;$interactionReply[Can't download this.]]
